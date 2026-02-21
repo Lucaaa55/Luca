4de446602b87f97b3778db1857cb8b79c5298e74
Luca <lucapignataro.p@gmail.com>
2026-02-21 12:08:18 -0300

    Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
     return (
         <>
             <main className={`flex min-h-screen flex-col items-center justify-between`}>
-                <div className={`flex flex-col items-center justify-center w-screen h-screen bg-linear-to-r from-[#e6eaf5] to-[#f6f6f6]`}>
-                    <h1>Hello World</h1>
+                <div className={`flex flex-col items-center justify-center w-screen h-screen bg-white`}>
+                    <h1>Under development...</h1>
                 </div>
             </main>
         </>
